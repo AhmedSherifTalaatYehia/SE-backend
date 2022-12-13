784f1d8bacd25644b242776219273a302c5b2c39
AhmedSherifTalaatYehia <ahmedsherif1591997@gmail.com>
2022-12-13 19:48:24 +0200

    little update

--- Tutorial10/PostgresSqlDB/tables.sql
+++ Tutorial10/PostgresSqlDB/tables.sql
@@ -9,7 +9,7 @@ create table Employees(
 
     id serial primary key,
     name text not null,
-    salary integer,
+    salary integer default 0,
     departmentId integer not null,
     constraint fk_emp_dep foreign key(departmentId) references Departments(id)
 );
